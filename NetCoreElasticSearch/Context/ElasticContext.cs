f9edf687a12dbb96b9c65aa091e615c399b8370c
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2023-07-01 16:45:39 +0300

    Add project files.

---
+++ NetCoreElasticSearch/Context/ElasticContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreElasticSearch.Context
+{
+    public class ElasticContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=.;database=Elastic;integrated security=true;");
+        }
+        public DbSet<Travel> Travels { get; set; }
+    }
+}
---
+++ NetCoreElasticSearch/Travel.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreElasticSearch
+{
+    public class Travel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
